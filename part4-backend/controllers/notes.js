acf066ce6f05fa3aee1c754170ced79205cdea5b
Byron David <byrondavid87@gmail.com>
2024-07-26 17:08:26 -0700

    Added tests/test_helper.js and imported it into note_api.test.js

--- part4-backend/controllers/notes.js
+++ part4-backend/controllers/notes.js
@@ -1,11 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
+notesRouter.get('/', async (request, response) => { 
+    const notes = await Note.find({})
     response.json(notes)
   })
-})
 
 notesRouter.get('/:id', (request, response, next) => {
   Note.findById(request.params.id)
@@ -29,7 +28,7 @@ notesRouter.post('/', (request, response, next) => {
 
   note.save()
     .then(savedNote => {
-      response.json(savedNote)
+        response.status(201).json(savedNote)
     })
     .catch(error => next(error))
 })
